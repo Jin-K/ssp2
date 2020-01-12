53a3b2c9bd4692d5277e6a872bd3a8997504743d
Angel Muñoz <angelmnz@outlook.com>
2020-01-12 15:09:28 +0100

    optimised store-helper to create actions with prefixes

--- src/app/@custom/pages/player/state/player.actions.ts
+++ src/app/@custom/pages/player/state/player.actions.ts
@@ -1,8 +1,8 @@
 import { props } from '@ngrx/store';
-import { createAction2, createAction } from '../../../utils';
+import { createPrefixedAction } from '../../../utils';
 import { Player } from '../player.service';
 
-const ACTIONS_PREFIX = '[PLAYER] ';
+const PREFIX = '[PLAYER] ';
 
 enum FormActionLabels {
   GET_PLAYERS = 'GET_PLAYERS',
@@ -14,12 +14,12 @@ enum FormActionLabels {
 }
 
 export const playerActions = {
-  GetPlayers: createAction(ACTIONS_PREFIX, FormActionLabels.GET_PLAYERS),
-  GetPlayersSucceeded: createAction2(ACTIONS_PREFIX, FormActionLabels.GET_PLAYERS_SUCCEEDED, props<{ items: Player[] }>()),
-  GetPlayersFailed: createAction2(ACTIONS_PREFIX, FormActionLabels.GET_PLAYERS_FAILED, props<object>()),
-  SavePlayer: createAction2(ACTIONS_PREFIX, FormActionLabels.SAVE_PLAYER, props<{ readonly data: object }>()),
-  SavePlayerSucceeded: createAction2(ACTIONS_PREFIX, FormActionLabels.SAVE_PLAYER_SUCCEEDED, props<{ readonly response: object }>()),
-  SavePlayerFailed: createAction2(ACTIONS_PREFIX, FormActionLabels.SAVE_PLAYER_FAILED, props<object>()),
+  GetPlayers: createPrefixedAction(PREFIX, FormActionLabels.GET_PLAYERS),
+  GetPlayersSucceeded: createPrefixedAction(PREFIX, FormActionLabels.GET_PLAYERS_SUCCEEDED, props<{ items: Player[] }>()),
+  GetPlayersFailed: createPrefixedAction(PREFIX, FormActionLabels.GET_PLAYERS_FAILED, props<object>()),
+  SavePlayer: createPrefixedAction(PREFIX, FormActionLabels.SAVE_PLAYER, props<{ readonly data: object }>()),
+  SavePlayerSucceeded: createPrefixedAction(PREFIX, FormActionLabels.SAVE_PLAYER_SUCCEEDED, props<{ readonly response: object }>()),
+  SavePlayerFailed: createPrefixedAction(PREFIX, FormActionLabels.SAVE_PLAYER_FAILED, props<object>()),
 };
 
 export declare type PlayerAction
--- src/app/@custom/utils/store-helper.ts
+++ src/app/@custom/utils/store-helper.ts
@@ -1,13 +1,18 @@
-import { createAction as originalCreateAction, ActionCreator } from '@ngrx/store';
+import { createAction, ActionCreator } from '@ngrx/store';
 import { TypedAction } from '@ngrx/store/src/models';
 
-export function createAction<T extends string>(prefix: string, type: T): ActionCreator<T, () => TypedAction<T>> {
-  return originalCreateAction(`${prefix}${type}` as T);
-}
+declare type TypedActionWithoutProps<T extends string> = ActionCreator<T, () => TypedAction<T>>;
+declare type TypedActionWithProps<T extends string, P extends object> = ActionCreator<T, (props: P) => P & TypedAction<T>>;
 
-export function createAction2<T extends string, P extends object>(prefix: string, type: T, config: {
+export function createPrefixedAction<T extends string>(prefix: string, type: T): TypedActionWithoutProps<T>;
+export function createPrefixedAction<T extends string, P extends object>(prefix: string, type: T, config?: {
   _as: 'props',
   _p: P,
-}): ActionCreator<T, (props: P) => P & TypedAction<T>> {
-  return originalCreateAction(`${prefix}${type}` as T, config);
+}): TypedActionWithProps<T, P>;
+
+export function createPrefixedAction<T extends string, P extends object>(prefix: string, type: T, config?: {
+  _as: 'props',
+  _p: P,
+}): TypedActionWithoutProps<T> | TypedActionWithProps<T, P> {
+  return createAction(`${prefix}${type}` as T, config);
 }
